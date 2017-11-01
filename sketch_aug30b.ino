4d24e528aa35ffd252395f3ca102aab719e95c49
Ricardo959 <32526723+Ricardo959@users.noreply.github.com>
2017-11-01 18:11:35 -0300

    Correções

--- sketch_aug30b.ino
+++ sketch_aug30b.ino
@@ -5,35 +5,37 @@ public:
   float lastSample;
   float kp, ki, kd;      
   float P, I, D;
+  float somaPID;
   float setPoint;
-  unsigned long lastProcess;
+  unsigned long tAnterior;
   
-  calculoPID(float _kp, float _ki, float _kd){
+  calculoPID(float _kp, float _ki, float _kd, float _setPoint) {
     kp = _kp;
     ki = _ki;
     kd = _kd;
-    lastProcess = 0;
-  }
-  
-  void addNewSample(float _sample){
-    sample = _sample;
-  }
-  
-  void setSetPoint(float _setPoint){
     setPoint = _setPoint;
+    tAnterior = 0;
+    I = 0;
   }
   
-  float pid(){
+  void addNewSample(float _sample) {
+    sample = _sample;
     error = setPoint - sample;
-    unsigned long deltaTime = (millis() - lastProcess) / 1000.0;
-    lastProcess = millis();
+
+    unsigned long t = millis();
+    int dt = t - tAnterior;
+    tAnterior = t;
       
     P = error * kp;    
-    I = I + (error * ki) * deltaTime;
-    D = (lastSample - sample) * kd / deltaTime;
+    I = I + (error * ki) * dt;
+    D = (lastSample - sample) * kd / dt;
     
-    lastSample = sample;  
-    float somaPID = P + I + D;
+    lastSample = sample; 
+  }
+  
+  float pid() {
+     
+    somaPID = P + I + D;
     
     return somaPID;
   }
@@ -54,6 +56,8 @@ int LDR_Esquerda = A2;
 int LDR_Direita = A3;
 int pont1 = A4; // potenciometro 1
 int pont2 = A5; // potenciometro 2
+int servoVerticalPin = 6;
+int servoHorizontalPin = 5;
 
 float vpont1 = 0;// variável de leitura dos potenciometros
 float vpont2 = 0;
@@ -67,13 +71,13 @@ float erroHorizontal = 0;
 float iVertical = 0;
 float iHorizontal = 0;
 
-float kpVertical = 0.0007;
-float kiVertical = 0.000045;
-float kdVertical = 0.15;
+float kpVertical = 0.2;
+float kiVertical = 0.000001;
+float kdVertical = 0.0001;
 
-float kpHorizontal = 0.0015;
-float kiHorizontal = 0.000045;
-float kdHorizontal = 0.15;
+float kpHorizontal = 0.2;
+float kiHorizontal = 0.000001;
+float kdHorizontal = 0.0001;
 
 
 unsigned long TAnterior = 0;
@@ -91,8 +95,8 @@ float leituraDireita = 0;
 Servo vertical;
 Servo horizontal;
 
-calculoPID pid_vertical(kpVertical, kiVertical, kdVertical);
-calculoPID pid_horizontal(kpHorizontal, kiHorizontal, kdHorizontal);
+calculoPID pid_vertical(kpVertical, kiVertical, kdVertical, 0);
+calculoPID pid_horizontal(kpHorizontal, kiHorizontal, kdHorizontal, 0);
 
 void setup() {
   Serial.begin(9600);
@@ -101,13 +105,10 @@ void setup() {
   digitalWrite(buttonPin, HIGH); //aciona o resistor putt-up interno
   vertical.attach(6);
   horizontal.attach(5);
-  
-  pid_vertical.setSetPoint(0);
-  pid_horizontal.setSetPoint(0);
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);// leia o estado do valor do botão de pressão:
+  buttonState = digitalRead(buttonPin); // leia o estado do valor do botão de pressão:
 
   if (buttonState == HIGH) {   // verifique se o botão está pressionado
     digitalWrite(ledPin, HIGH);  // liga o LED:
@@ -136,30 +137,74 @@ void loop() {
   }
 
 
-  float erroVertical = leituraCima - leituraBaixo;
-  float erroHorizontal = leituraEsquerda - leituraDireita;
+  float erroVertical =  leituraBaixo - leituraCima;
+  float erroHorizontal =  leituraDireita - leituraEsquerda;
 
   pid_vertical.addNewSample(erroVertical);
   pid_horizontal.addNewSample(erroHorizontal);
 
-  grau_vertical += pid_vertical.pid();
-  grau_horizontal += pid_horizontal.pid();
+  grau_vertical = grau_vertical + pid_vertical.pid();
+  grau_horizontal = grau_horizontal + pid_horizontal.pid();
 
-  if (abs(difVertical) > 3 && grau_vertical < 65 && grau_vertical > 3) {
-    vertical.write(int(grau_vertical));
-  } else if (grau_vertical > 65) {
+
+  //Checagem de limite de angulo
+  if (grau_vertical > 65) {
     grau_vertical = 65;
+    vertical.write(int(grau_vertical));
+    vertical.detach();
   } else if (grau_vertical < 3) {
     grau_vertical = 3;
+    vertical.write(int(grau_vertical));
+    vertical.detach();
+  } else if (!vertical.attached()) {
+    vertical.attach(servoVerticalPin);
   }
 
-  if (abs(difHorizontal) > 3 && grau_horizontal < 175 && grau_horizontal > 5) {
-    horizontal.write(int(grau_horizontal));
-  } else if (grau_horizontal > 175) {
+  //Checagem de diferença e escrita no servo
+  if (abs(erroVertical) > 3) {
+    if (!vertical.attached()) {
+      vertical.attach(servoVerticalPin);
+    }
+    vertical.write(int(grau_vertical));
+  } else {
+    vertical.detach();
+  }
+
+
+  //Checagem de limite de angulo
+  if (grau_horizontal > 175) {
     grau_horizontal = 175;
+    horizontal.write(int(grau_horizontal));
+    horizontal.detach();
   } else if (grau_horizontal < 5) {
     grau_horizontal = 5;
+    horizontal.write(int(grau_horizontal));
+    horizontal.detach();
+  } else if (!horizontal.attached()) {
+      horizontal.attach(servoHorizontalPin);
   }
+
+  //Checagem de diferença e escrita no servo
+  if (abs(erroHorizontal) > 3) {
+    if (!horizontal.attached()) {
+      horizontal.attach(servoHorizontalPin);
+    }
+    horizontal.write(int(grau_horizontal));
+  } else {
+    horizontal.detach();
+  }
+
+  Serial.print("erroVertical = ");
+  Serial.print(erroVertical);
+  Serial.print("      ");
+  Serial.print("grau_vertical = ");
+  Serial.print(grau_vertical);
+  Serial.print("      ");
+  Serial.print("grau_horizontal = ");
+  Serial.print(grau_horizontal);
+  Serial.print("      ");
+  Serial.print("pidVertical = ");
+  Serial.println(pid_vertical.pid());
 
   delay(1);
 }
