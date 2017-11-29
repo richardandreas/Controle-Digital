cd6ede2340fd061a167699f70f96f5822bc7fbf1
ThulioGuedes <32526661+ThulioGuedes@users.noreply.github.com>
2017-11-29 17:08:51 -0300

    Update sketch_aug30b.ino

--- sketch_aug30b.ino
+++ sketch_aug30b.ino
@@ -164,7 +164,7 @@ class calculoPID {
           Serial.println("Digite um valor para P: ");
           Serial.println(" ");
           Serial.print("Valor atual: ");
-          Serial.print(kp,8);
+          Serial.print(kp, 8);
           delay(6000);
           p = _P;
           _P = Serial.parseFloat();
@@ -181,7 +181,7 @@ class calculoPID {
           Serial.println("Digite um valor para I: ");
           Serial.println(" ");
           Serial.print("Valor atual: ");
-          Serial.print(ki,8);
+          Serial.print(ki, 8);
           delay(6000);
           i = _I;
           _I = Serial.parseFloat();
@@ -199,7 +199,7 @@ class calculoPID {
           Serial.println("Digite um valor para D: ");
           Serial.println(" ");
           Serial.print("Valor atual: ");
-          Serial.print(kd,8);
+          Serial.print(kd, 8);
           delay(6000);
           d = _D;
           _D = Serial.parseFloat();
@@ -225,248 +225,252 @@ class calculoPID {
           break;
       }
     }
-   void SetMode(int Mode)
-   {
-     int NewState = Mode;
-     int LastState = LOW;
-     if(NewState == LOW && !LastState == LOW)
-     {
-       Initialize();
-     }
-     LastState = NewState;
-   }
-   void Initialize()
-   {
-     lastsample = sample;
-     sumPID = sumError;
-     if(sumPID > OutMax)
-     {
-       sumPID = OutMax;
-     }
-     else if(sumPID < outMin)
-     {
-       sumPID = OutMin;
-     }        
-};
-#include <Servo.h>
-// o número do pino do botão
-const int buttonPin = 3;
-// o número do pino LED
-const int ledPin =  4;
-const int led1 = 7;
-const int led2 = 8;
-// variável para ler o status do botão de pressão
-int buttonState = 0;
-
-int LDR_Cima = A0;
-int LDR_Baixo = A1;
-int LDR_Esquerda = A2;
-int LDR_Direita = A3;
-int pont1 = A4; // potenciometro 1
-int pont2 = A5; // potenciometro 2
-int servoVerticalPin = 6;
-int servoHorizontalPin = 5;
-
-float vpont1 = 0;// variável de leitura dos potenciometros
-float vpont2 = 0;
-
-float grau_vertical = 0;
-float grau_horizontal = 0;
-
-float erroVertical = 0;
-float erroHorizontal = 0;
-
-float iVertical = 0;
-float iHorizontal = 0;
-
-float kpVertical = 0.3;
-float kiVertical = 0.00001; // Quanto maior o ki, mais lento a reação do servo
-float kdVertical = 0.0001; // Quanto maior o kd, mais instável o movimento do servo
-
-float kpHorizontal = 0.3;
-float kiHorizontal = 0.00001;
-float kdHorizontal = 0.0001;
-
-
-unsigned long TAnterior = 0;
-
-
-float difVerticalAnterior = 0;
-float difHorizontalAnterior = 0;
-
-
-float leituraCima = 0;
-float leituraBaixo = 0;
-float leituraEsquerda = 0;
-float leituraDireita = 0;
-
-Servo vertical;
-Servo horizontal;
-
-calculoPID pid_vertical(kpVertical, kiVertical, kdVertical, 0);
-calculoPID pid_horizontal(kpHorizontal, kiHorizontal, kdHorizontal, 0);
-
-char lerpid, inByte;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT); //pino de saida do led
-  pinMode(buttonPin, INPUT); //pino de entrada do botão
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  digitalWrite(buttonPin, HIGH); //aciona o resistor putt-up interno
-  vertical.attach(6);
-  horizontal.attach(5);
-
-  pid_vertical.SetTuningsST(50);
-  pid_horizontal.SetTuningsST(50);
-
-  pid_vertical.SetOutputLimits(-5, 5);
-  pid_horizontal.SetOutputLimits(-5, 5);
-}
-
-void loop() {
-  buttonState = digitalRead(buttonPin); // leia o estado do valor do botão de pressão:
-  
-  pid_vertical.SetMode(buttonState);  // Fornece o Status do botão para a função SetMode
-  pid_horizontal.SetMode(buttonState);  // Fornece o Status do botão para a função SetMode
-  
-  if (buttonState == HIGH) {   // verifique se o botão está pressionado
-    digitalWrite(ledPin, HIGH);  // liga o LED:
-
-    vpont1 = analogRead(pont1);  //capturando valores analogicos de cada Potenciometro
-    vpont1 = map(vpont1, 0, 1020, 3, 65);
-
-    vpont2 = analogRead(pont2);
-    vpont2 = map(vpont2, 0, 1020, 4, 175);
-
-    erroVertical = grau_vertical - vpont1;
-    erroHorizontal = grau_horizontal - vpont2;
-
-  } else {
-
-    digitalWrite(ledPin, LOW); // desliga o LED:
-    leituraCima = analogRead(LDR_Cima);
-    leituraCima = map(leituraCima, 10, 1019, 0, 100);
-    leituraBaixo = analogRead(LDR_Baixo);
-    leituraBaixo = map(leituraBaixo, 10, 1019, 0, 100);
-
-    leituraEsquerda = analogRead(LDR_Esquerda);
-    leituraEsquerda = map(leituraEsquerda, 10, 1019, 0, 100);
-    leituraDireita = analogRead(LDR_Direita);
-    leituraDireita = map(leituraDireita, 10, 1019, 0, 100);
-
-    erroVertical =  leituraBaixo - leituraCima;
-    erroHorizontal =  leituraDireita - leituraEsquerda;
-  }
-
-  pid_vertical.addNewSample(erroVertical);
-  pid_vertical.Compute();
-  pid_horizontal.addNewSample(erroHorizontal);
-  pid_horizontal.Compute();
-
-  pid_vertical.ComputeRW();
-  pid_horizontal.ComputeRW();
-
-  grau_vertical = grau_vertical + pid_vertical.pid();
-  grau_horizontal = grau_horizontal + pid_horizontal.pid();
-
-  //Checagem de limite de angulo
-
-  if (grau_vertical > 65) {
-    grau_vertical = 65;
-    vertical.write(int(grau_vertical));
-    vertical.detach();
-  } else if (grau_vertical < 3) {
-    grau_vertical = 3;
-    vertical.write(int(grau_vertical));
-    vertical.detach();
-  } else if (!vertical.attached()) {
-    vertical.attach(servoVerticalPin);
-  }
-
-  //Checagem de diferença e escrita no servo
-
-  if (abs(erroVertical) > 3) {
-    if (!vertical.attached()) {
-      vertical.attach(servoVerticalPin);
-    }
-    vertical.write(int(grau_vertical));
-  } else {
-    vertical.detach();
-  }
-
-  //Checagem de limite de angulo
-
-  if (grau_horizontal > 175) {
-    grau_horizontal = 175;
-    horizontal.write(int(grau_horizontal));
-    horizontal.detach();
-  } else if (grau_horizontal < 5) {
-    grau_horizontal = 5;
-    horizontal.write(int(grau_horizontal));
-    horizontal.detach();
-  } else if (!horizontal.attached()) {
-    horizontal.attach(servoHorizontalPin);
-  }
-
-  //Checagem de diferença e escrita no servo
-
-  if (abs(erroHorizontal) > 3) {
-    if (!horizontal.attached()) {
-      horizontal.attach(servoHorizontalPin);
-    }
-    horizontal.write(int(grau_horizontal));
-  } else {
-    horizontal.detach();
-  }
-
-  // entrada de valores para p i d
-  if (Serial.available() > 0) {
-    lerpid = Serial.read();
-    if (lerpid == 'l') {
-      Serial.println("Selecione o eixo: v || h");
-      digitalWrite(led1, HIGH);
-      digitalWrite(led2, HIGH);
-      delay(1000);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      delay(2000);
-      lerpid = Serial.read();
-      if (lerpid == 'v') {
-        Serial.println("Selecione: p || i || d");
-        digitalWrite(led1, HIGH);
-        digitalWrite(led2, HIGH);
-        delay(1000);
-        digitalWrite(led1, LOW);
-        digitalWrite(led2, LOW);
-        delay(2000);
-        inByte = Serial.read();
-        pid_vertical.LER(inByte);
-      } else if (lerpid == 'h') {
-        Serial.println("Selecione: p || i || d");
-        digitalWrite(led1, HIGH);
-        digitalWrite(led2, HIGH);
-        delay(1000);
-        digitalWrite(led1, LOW);
-        digitalWrite(led2, LOW);
-        delay(2000);
-        inByte = Serial.read();
-        pid_horizontal.LER(inByte);
+    void SetMode(int Mode)
+    {
+      int NewState = Mode;
+      int LastState = LOW;
+      if (NewState == LOW && !LastState == LOW)
+      {
+        Initialize();
       }
+      LastState = NewState;
     }
-  }
+    void Initialize()
+    {
+      lastsample = sample;
+      sumPID = sumError;
+      if (sumPID > OutMax)
+      {
+        sumPID = OutMax;
+      }
+      else if (sumPID < outMin)
+      {
+        sumPID = OutMin;
+      }
 
-  Serial.print("erroVertical = ");
-  Serial.print(erroVertical);
-  Serial.print("      ");
-  Serial.print("grau_vertical = ");
-  Serial.print(grau_vertical);
-  Serial.print("      ");
-  Serial.print("leituraCima = ");
-  Serial.print(leituraCima);
-  Serial.print("      ");
-  Serial.print("pidVertical = ");
-  Serial.println(pid_vertical.pid());
-  
-}
+    };
+
+
+#include <Servo.h>
+    // o número do pino do botão
+    const int buttonPin = 3;
+    // o número do pino LED
+    const int ledPin =  4;
+    const int led1 = 7;
+    const int led2 = 8;
+    // variável para ler o status do botão de pressão
+    int buttonState = 0;
+
+    int LDR_Cima = A2;
+    int LDR_Baixo = A1;
+    int LDR_Esquerda = A0;
+    int LDR_Direita = A3;
+    int pont1 = A4; // potenciometro 1
+    int pont2 = A5; // potenciometro 2
+    int servoVerticalPin = 6;
+    int servoHorizontalPin = 5;
+
+    float vpont1 = 0;// variável de leitura dos potenciometros
+    float vpont2 = 0;
+
+    float grau_vertical = 0;
+    float grau_horizontal = 0;
+
+    float erroVertical = 0;
+    float erroHorizontal = 0;
+
+    float iVertical = 0;
+    float iHorizontal = 0;
+
+    float kpVertical = 0.23;
+    float kiVertical = 0.000000023; // Quanto maior o ki, mais lento a reação do servo
+    float kdVertical = 0.00000023; // Quanto maior o kd, mais instável o movimento do servo
+
+    float kpHorizontal = 0.23;
+    float kiHorizontal = 0.00000023;
+    float kdHorizontal = 0.0000023;
+
+
+    unsigned long TAnterior = 0;
+
+
+    float difVerticalAnterior = 0;
+    float difHorizontalAnterior = 0;
+
+
+    float leituraCima = 0;
+    float leituraBaixo = 0;
+    float leituraEsquerda = 0;
+    float leituraDireita = 0;
+
+    Servo vertical;
+    Servo horizontal;
+
+    calculoPID pid_vertical(kpVertical, kiVertical, kdVertical, 0);
+    calculoPID pid_horizontal(kpHorizontal, kiHorizontal, kdHorizontal, 0);
+
+    char lerpid, inByte;
+
+    void setup() {
+      Serial.begin(9600);
+      pinMode(ledPin, OUTPUT); //pino de saida do led
+      pinMode(buttonPin, INPUT); //pino de entrada do botão
+      pinMode(led1, OUTPUT);
+      pinMode(led2, OUTPUT);
+      digitalWrite(buttonPin, HIGH); //aciona o resistor putt-up interno
+      vertical.attach(6);
+      horizontal.attach(5);
+
+      pid_vertical.SetTuningsST(50);
+      pid_horizontal.SetTuningsST(50);
+
+      pid_vertical.SetOutputLimits(-5, 5);
+      pid_horizontal.SetOutputLimits(-5, 5);
+    }
+
+    void loop() {
+      buttonState = digitalRead(buttonPin); // leia o estado do valor do botão de pressão:
+
+      if (buttonState == HIGH) {   // verifique se o botão está pressionado
+        digitalWrite(ledPin, HIGH);  // liga o LED:
+
+        vpont1 = analogRead(pont1);  //capturando valores analogicos de cada Potenciometro
+        vpont1 = map(vpont1, 0, 1020, 3, 65);
+
+        vpont2 = analogRead(pont2);
+        vpont2 = map(vpont2, 0, 1020, 4, 175);
+
+        erroVertical = grau_vertical - vpont1;
+        erroHorizontal = grau_horizontal - vpont2;
+
+      } else {
+
+        digitalWrite(ledPin, LOW); // desliga o LED:
+        leituraCima = analogRead(LDR_Cima);
+        leituraCima = map(leituraCima, 10, 1019, 0, 100);
+        leituraBaixo = analogRead(LDR_Baixo);
+        leituraBaixo = map(leituraBaixo, 10, 1019, 0, 100);
+
+        leituraEsquerda = analogRead(LDR_Esquerda);
+        leituraEsquerda = map(leituraEsquerda, 10, 1019, 0, 100);
+        leituraDireita = analogRead(LDR_Direita);
+        leituraDireita = map(leituraDireita, 10, 1019, 0, 100);
+
+        erroVertical =  leituraBaixo - leituraCima;
+        erroHorizontal =  leituraDireita - leituraEsquerda;
+      }
+
+      pid_vertical.addNewSample(erroVertical);
+      pid_vertical.Compute();
+      pid_horizontal.addNewSample(erroHorizontal);
+      pid_horizontal.Compute();
+
+      pid_vertical.ComputeRW();
+      pid_horizontal.ComputeRW();
+
+      grau_vertical = grau_vertical + pid_vertical.pid();
+      grau_horizontal = grau_horizontal + pid_horizontal.pid();
+
+      //Checagem de limite de angulo
+
+      if (grau_vertical > 65) {
+        grau_vertical = 65;
+        vertical.write(int(grau_vertical));
+        vertical.detach();
+      } else if (grau_vertical < 3) {
+        grau_vertical = 3;
+        vertical.write(int(grau_vertical));
+        vertical.detach();
+      } else if (!vertical.attached()) {
+        vertical.attach(servoVerticalPin);
+      }
+
+      //Checagem de diferença e escrita no servo
+
+      if (abs(erroVertical) > 3) {
+        if (!vertical.attached()) {
+          vertical.attach(servoVerticalPin);
+        }
+        vertical.write(int(grau_vertical));
+      } else {
+        vertical.detach();
+      }
+
+      //Checagem de limite de angulo
+
+      if (grau_horizontal > 175) {
+        grau_horizontal = 175;
+        horizontal.write(int(grau_horizontal));
+        horizontal.detach();
+      } else if (grau_horizontal < 5) {
+        grau_horizontal = 5;
+        horizontal.write(int(grau_horizontal));
+        horizontal.detach();
+      } else if (!horizontal.attached()) {
+        horizontal.attach(servoHorizontalPin);
+      }
+
+      //Checagem de diferença e escrita no servo
+
+      if (abs(erroHorizontal) > 3) {
+        if (!horizontal.attached()) {
+          horizontal.attach(servoHorizontalPin);
+        }
+        horizontal.write(int(grau_horizontal));
+      } else {
+        horizontal.detach();
+      }
+
+      // entrada de valores para p i d
+      if (Serial.available() > 0) {
+        lerpid = Serial.read();
+        if (lerpid == 'l') {
+          Serial.println("Selecione o eixo: v || h");
+          digitalWrite(led1, HIGH);
+          digitalWrite(led2, HIGH);
+          delay(1000);
+          digitalWrite(led1, LOW);
+          digitalWrite(led2, LOW);
+          delay(2000);
+          lerpid = Serial.read();
+          if (lerpid == 'v') {
+            Serial.println("Selecione: p || i || d");
+            digitalWrite(led1, HIGH);
+            digitalWrite(led2, HIGH);
+            delay(1000);
+            digitalWrite(led1, LOW);
+            digitalWrite(led2, LOW);
+            delay(2000);
+            inByte = Serial.read();
+            pid_vertical.LER(inByte);
+          } else if (lerpid == 'h') {
+            Serial.println("Selecione: p || i || d");
+            digitalWrite(led1, HIGH);
+            digitalWrite(led2, HIGH);
+            delay(1000);
+            digitalWrite(led1, LOW);
+            digitalWrite(led2, LOW);
+            delay(2000);
+            inByte = Serial.read();
+            pid_horizontal.LER(inByte);
+          }
+        }
+      }
+
+      Serial.print("erroVertical = ");
+      Serial.print(erroVertical);
+      Serial.print("      ");
+      Serial.print("grau_vertical = ");
+      Serial.print(grau_vertical);
+      Serial.print("      ");
+      Serial.print("erroHorizontal = ");
+      Serial.print(erroHorizontal);
+      Serial.print("      ");
+      Serial.print("grau_horizontal = ");
+      Serial.print(grau_horizontal);
+      Serial.print("      ");
+      Serial.print("pidVertical = ");
+      Serial.println(pid_vertical.pid());
+
+      delay(1);
+    }
